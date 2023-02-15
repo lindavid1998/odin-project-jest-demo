01860dba68532483afa3b93b879a50a5976e9b83
David Lin <lindavid1998@gmail.com>
2023-02-15 07:26:18 -0800

    Implement analyzeArray

--- assignment.js
+++ assignment.js
@@ -1,54 +1,66 @@
 function capitalize(string) {
-  return string[0].toUpperCase() + string.slice(1);
+	return string[0].toUpperCase() + string.slice(1);
 }
 
 function reverseString(string) {
-  let strArray = string.split('');
-  return strArray.reverse().join('');
+	let strArray = string.split('');
+	return strArray.reverse().join('');
 }
 
 const calculator = {
-  add(a, b) {
-    return a + b;
-  },
-  subtract(a, b) {
-    return a - b;
-  },
-  divide(a, b) {
-    return a / b;
-  },
-  multiply(a, b) {
-    return a * b;
-  },
+	add(a, b) {
+		return a + b;
+	},
+	subtract(a, b) {
+		return a - b;
+	},
+	divide(a, b) {
+		return a / b;
+	},
+	multiply(a, b) {
+		return a * b;
+	},
 };
 
 function caesarCipher(string, key) {
-  key = key % 26; // normalize key to be between 0 and 25
-  let arr = [];
-  let charCode;
-  for (let i = 0; i < string.length; i++) {
-    charCode = string[i].charCodeAt();
+	key = key % 26; // normalize key to be between 0 and 25
+	let arr = [];
+	let charCode;
+	for (let i = 0; i < string.length; i++) {
+		charCode = string[i].charCodeAt();
 
-    if (charCode > 96 && charCode < 123) {
-      charCode += key;
-      if (charCode > 122) {
-        charCode = 96 + (charCode - 122);
-      }
-    } else if (charCode > 64 && charCode < 91) {
-      charCode += key;
-      if (charCode > 90) {
-        charCode = 64 + (charCode - 90);
-      }
-    }
+		if (charCode > 96 && charCode < 123) {
+			charCode += key;
+			if (charCode > 122) {
+				charCode = 96 + (charCode - 122);
+			}
+		} else if (charCode > 64 && charCode < 91) {
+			charCode += key;
+			if (charCode > 90) {
+				charCode = 64 + (charCode - 90);
+			}
+		}
 
-    arr.push(charCode);
-  }
+		arr.push(charCode);
+	}
 
-  let outputArr = arr.map((code) => {
-    return String.fromCharCode(code);
-  });
+	let outputArr = arr.map((code) => {
+		return String.fromCharCode(code);
+	});
 
-  return outputArr.join('');
+	return outputArr.join('');
 }
 
-export { capitalize, reverseString, calculator, caesarCipher };
+function analyzeArray(arr) {
+	let output = {};
+
+	let sum = arr.reduce((acc, current) => acc + current, 0);
+	output['average'] = sum / arr.length;
+	output['min'] = Math.min(...arr);
+	output['max'] = Math.max(...arr);
+	output['length'] = arr.length;
+
+	return output;
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- assignment.test.js
+++ assignment.test.js
@@ -1,57 +1,74 @@
 import {
-  capitalize,
-  reverseString,
-  calculator,
-  caesarCipher,
+	capitalize,
+	reverseString,
+	calculator,
+	caesarCipher,
+	analyzeArray,
 } from './assignment';
 
 test('Returns string with first char capitalized', () => {
-  expect(capitalize('foo')).toBe('Foo');
+	expect(capitalize('foo')).toBe('Foo');
 });
 
 test('Returns reversed input string', () => {
-  expect(reverseString('haha')).toBe('ahah');
-  expect(reverseString('test')).toBe('tset');
-  expect(reverseString('racecar')).toBe('racecar');
+	expect(reverseString('haha')).toBe('ahah');
+	expect(reverseString('test')).toBe('tset');
+	expect(reverseString('racecar')).toBe('racecar');
 });
 
 test('Adds two numbers', () => {
-  expect(calculator.add(1, 2)).toBe(3);
-  expect(calculator.add(-3, 4)).toBe(1);
-  expect(calculator.add(0, 10)).toBe(10);
+	expect(calculator.add(1, 2)).toBe(3);
+	expect(calculator.add(-3, 4)).toBe(1);
+	expect(calculator.add(0, 10)).toBe(10);
 });
 
 test('Subtracts two numbers', () => {
-  expect(calculator.subtract(2, 1)).toBe(1);
-  expect(calculator.subtract(4, 2)).toBe(2);
+	expect(calculator.subtract(2, 1)).toBe(1);
+	expect(calculator.subtract(4, 2)).toBe(2);
 });
 
 test('Divides two numbers', () => {
-  expect(calculator.divide(4, 2)).toBe(2);
-  expect(calculator.divide(7, 4)).toBe(7 / 4);
+	expect(calculator.divide(4, 2)).toBe(2);
+	expect(calculator.divide(7, 4)).toBe(7 / 4);
 });
 
 test('Multiplies two numbers', () => {
-  expect(calculator.multiply(4, 2)).toBe(8);
-  expect(calculator.multiply(4, -2)).toBe(-8);
-  expect(calculator.multiply(7, 4)).toBe(28);
-  expect(calculator.multiply(-3, -7)).toBe(21);
+	expect(calculator.multiply(4, 2)).toBe(8);
+	expect(calculator.multiply(4, -2)).toBe(-8);
+	expect(calculator.multiply(7, 4)).toBe(28);
+	expect(calculator.multiply(-3, -7)).toBe(21);
 });
 
 test('Ceasar scipher shifts string using key', () => {
-  expect(caesarCipher('test', 0)).toBe('test');
-  expect(caesarCipher('test', 1)).toBe('uftu');
-  expect(caesarCipher('hello', 3)).toBe('khoor');
-  expect(caesarCipher('test', 26)).toBe('test');
-  expect(caesarCipher('test', 27)).toBe('uftu');
+	expect(caesarCipher('test', 0)).toBe('test');
+	expect(caesarCipher('test', 1)).toBe('uftu');
+	expect(caesarCipher('hello', 3)).toBe('khoor');
+	expect(caesarCipher('test', 26)).toBe('test');
+	expect(caesarCipher('test', 27)).toBe('uftu');
 });
 
 test('Caesar cipher ignores spaces and punctuation', () => {
-  expect(caesarCipher('test!', 1)).toBe('uftu!');
-  expect(caesarCipher('hey there!', 5)).toBe('mjd ymjwj!');
+	expect(caesarCipher('test!', 1)).toBe('uftu!');
+	expect(caesarCipher('hey there!', 5)).toBe('mjd ymjwj!');
 });
 
 test('Caesar cipher wraps from z to a', () => {
-  expect(caesarCipher('zookeeper', 1)).toBe('applffqfs');
-  expect(caesarCipher('Zebra', 3)).toBe('Cheud');
+	expect(caesarCipher('zookeeper', 1)).toBe('applffqfs');
+	expect(caesarCipher('Zebra', 3)).toBe('Cheud');
+});
+
+test('Returns average of array', () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
+
+test('Returns min of array', () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe(1);
+});
+
+test('Returns max of array', () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe(8);
+});
+
+test('Returns length of array', () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe(6);
 });
